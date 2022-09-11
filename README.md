c5b752a2c6d9e0f09338f782f37d23ebce1f322f
Juan Acevedo <jfacevedo@google.com>
2022-09-11 12:21:23 +0000

    add img2img

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# serving-diffusion - WIP
+# serving-diffusion
 
 This repo creates a serving container for stable diffusion.
 
@@ -27,6 +27,7 @@ This repo creates a serving container for stable diffusion.
 1. Make a prediction
 
   ```bash
+  python generate_request.py
   curl -X POST -d @request.json -H "Content-Type: application/json; charset=utf-8" localhost/predict >> response.json
   ```
   
@@ -37,6 +38,8 @@ This repo creates a serving container for stable diffusion.
     ```
 
 1. Test the endpoint.
+
+    **Txt2img**
 
     ```python
     from google.cloud import aiplatform
@@ -54,11 +57,50 @@ This repo creates a serving container for stable diffusion.
         'H' : 512, 
         'ddim_steps' : 50,
         'n_samples' : 2,
-        'n_iter' : 2
+        'n_iter' : 2,
+        'type' : 'txt2img'
     }
     parameters = json_format.ParseDict(parameters,Value())
 
     results = endpoint.predict(instances=instances,parameters=parameters)
 
     ```
+
+    **Img2img**
+
+    ```python
+         from google.cloud import aiplatform
+
+    from google.protobuf import json_format
+    from google.protobuf.struct_pb2 import Value
+
+    from PIL import Image
+
+    def im_2_b64(image, format='PNG'):
+        buff = BytesIO()
+        image.save(buff, format=format)
+        img_str = base64.b64encode(buff.getvalue())
+        return img_str
+
+    image = Image.open("image.png")
+
+    base64_image = im_2_b64(image).decode('utf-8')
+
+    instances_list = [{"prompt": "A dog wearing a hat", 'image' : base64_image}]
+    instances = [json_format.ParseDict(s, Value()) for s in instances_list]
+
+    parameters = {
+        'strength' : .55,
+        'scale' : 7.5, 
+        'seed' : 42, 
+        'W' : 512, 
+        'H' : 512, 
+        'ddim_steps' : 50,
+        'n_samples' : 2,
+        'n_iter' : 2,
+        'type' : 'img2img'
+    }
+    parameters = json_format.ParseDict(parameters,Value())
+
+    results = endpoint.predict(instances=instances,parameters=parameters)
 
--- app/main.py
+++ app/main.py
@@ -26,6 +26,7 @@ from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
 
 from txt2img import txt2img
+from img2img import img2img
 
 import logging
 logger = logging.getLogger()
@@ -56,8 +57,6 @@ config = OmegaConf.load("configs/stable-diffusion/v1-inference.yaml")
 model = load_model_from_config(config, "models/ldm/stable-diffusion-v1/model.ckpt")
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
-sampler = PLMSSampler(model)
 
 txt2img_outdir = "/outputs/txt2img-samples"
 img2img_outdir = "/outputs/img2img-samples"
@@ -94,82 +93,25 @@ async def predict(request: Request):
     config =  body["parameters"]
     logger.debug(f"config : {config}")
 
-    # seed = config.get('seed',DEFAULT_SEED)
-    # seed_everything(seed)
-
-    # n_samples = config.get('n_samples', DEFAULT_N_SAMPLES)
-    # batch_size = n_samples
-    # n_iter = config.get('n_iter', DEFAULT_N_ITER)
-
-    # ddim_steps = config.get('ddim_steps',DEFAULT_DDIM_STEPS)
-    # scale = config.get('scale',DEFAULT_SCALE)
-    # ddim_eta = config.get('ddim_eta',DEFAULT_DDIM_ETA)
-
-    # C = config.get('C',DEFAULT_C)
-    # H = config.get('H',DEFAULT_H)
-    # W = config.get('W',DEFAULT_W)
-    # f = config.get('f',DEFAULT_F)
-
     infer_type = config.get('type',"txt2img")
-
+    
+    prompt = instances[0]["prompt"]
     if infer_type == 'txt2img':
-        prompt = instances[0]["prompt"]
+        sampler = PLMSSampler(model)
         retval = txt2img(model=model,
                         sampler=sampler,
                         prompt=prompt,
                         config=config,
                         outpath=txt2img_outdir)
-    #elif infer_type == 'img2img':
+
+    elif infer_type == 'img2img':
+        image = instances[0]["image"]
+        sampler = DDIMSampler(model)
+        retval = img2img(model=model,
+                        sampler=sampler,
+                        image=image,
+                        prompt=prompt,
+                        config=config,
+                        outpath=img2img_outdir)
+
     return {"predictions" : retval}
-    # #prompt = instances[0]["prompt"]
-    # data = [batch_size * [prompt]]
-
-    # sample_path = os.path.join(outpath, "samples")
-    # os.makedirs(sample_path, exist_ok=True)
-    # base_count = 0
-
-    # unique_id = str(uuid.uuid4())[:8]
-
-    # start_code = None
-    # precision_scope = autocast
-    # with torch.no_grad():
-    #     with precision_scope("cuda"):
-    #         with model.ema_scope():
-    #             tic = time.time()
-    #             for n in trange(n_iter, desc="Sampling"):
-    #                 for prompts in tqdm(data, desc="data"):
-    #                     uc = model.get_learned_conditioning(batch_size * [""])
-    #                     if isinstance(prompts,tuple):
-    #                         prompts = list(prompts)
-    #                     c = model.get_learned_conditioning(prompts)
-    #                     shape = [C, H // f, W // f]
-    #                     samples_ddim, _ = sampler.sample(S=ddim_steps,
-    #                                                     conditioning=c,
-    #                                                     batch_size=n_samples,
-    #                                                     shape=shape,
-    #                                                     verbose=False,
-    #                                                     unconditional_guidance_scale=scale,
-    #                                                     unconditional_conditioning=uc,
-    #                                                     eta=ddim_eta,
-    #                                                     x_t=start_code
-    #                                                     )
-    #                     x_samples_ddim = model.decode_first_stage(samples_ddim)
-    #                     x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-
-    #                     for x_sample in x_samples_ddim:
-    #                         x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-    #                         Image.fromarray(x_sample.astype(np.uint8)).save(
-    #                             os.path.join(outpath,f"{unique_id}-{base_count:05}.png"))
-    #                         base_count += 1
-
-    #             toc = time.time()
-
-    # retval = []          
-    # for i in range(base_count-1,-1,-1):
-    #     img_path = os.path.join(outpath,f"{unique_id}-{i:05}.png")
-    #     with open(img_path, "rb") as image_file:
-    #         print("encoding image")
-    #         base64_image = base64.b64encode(image_file.read())
-    #         retval.append(base64_image)
-    
-    # return {"predictions" : retval}
